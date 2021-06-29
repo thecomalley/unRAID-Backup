54af2ff3a79f5c90e65c189da0e4e3d2a5838a6f
thecomalley <31399219+thecomalley@users.noreply.github.com>
2021-06-29 14:43:59 +1000

    add healthchecks.io

--- diagrams/diagram.py
+++ diagrams/diagram.py
@@ -1,4 +1,6 @@
-from diagrams import Cluster, Diagram
+from typing import Text
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
 from diagrams.azure.database import BlobStorage
 from diagrams.generic.storage import Storage
 from diagrams.programming.language import Bash
@@ -13,4 +15,8 @@ with Diagram("Homelab Remote Backup", show=False):
         with Cluster("Storage-Account"):
             appdata = BlobStorage("unraid-appdata")
 
+    healthchecks = Custom("healthchecks", "./healthchecksio.png")
+
     unraid >> rclone >> appdata
+
+    rclone << Edge(color="firebrick", style="dashed") << healthchecks

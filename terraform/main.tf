1c30c89fe40af583ea95ac8ccb2ecc879c17f5b4
thecomalley <31399219+thecomalley@users.noreply.github.com>
2021-06-29 21:14:58 +1000

    add template_dir to make tpl files

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,25 @@ module "naming" {
 
 locals {
   common_tags = {
-    Service = "local.service_name"
-    Owner   = "local.owner"
+    application = "unRAID-backup"
+    enviroment  = "production"
+    deployment  = "terraform"
   }
 }
 
 resource "azurerm_resource_group" "example" {
   name     = module.naming.resource_group.name
   location = var.location
+  tags     = local.common_tags
+}
+
+resource "template_dir" "config" {
+  source_dir      = "../rclone/templates"
+  destination_dir = "../rclone/user_scripts"
+
+  vars = {
+    ping_url           = "${healthchecksio_check.appdata.ping_url}"
+    storage_account    = "${azurerm_storage_account.example.name}"
+    primary_access_key = "${azurerm_storage_account.example.primary_access_key}"
+  }
 }

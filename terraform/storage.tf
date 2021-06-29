17aec2f624992b5912f8ef52927862a138fb76f0
thecomalley <31399219+thecomalley@users.noreply.github.com>
2021-06-29 21:11:57 +1000

    add Lifecycle management

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -4,14 +4,30 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-  tags = {
-    environment = "staging"
-  }
+  tags                     = local.common_tags
 }
 
 resource "azurerm_storage_container" "example" {
   name                  = "appdata"
   storage_account_name  = azurerm_storage_account.example.name
   container_access_type = "private"
+}
+
+resource "azurerm_storage_management_policy" "example" {
+  storage_account_id = azurerm_storage_account.example.id
+  rule {
+    name    = "global-policy"
+    enabled = true
+    filters {
+      prefix_match = ["*"]
+      blob_types   = ["blockBlob"]
+    }
+    actions {
+      base_blob {
+        tier_to_cool_after_days_since_modification_greater_than    = 30
+        tier_to_archive_after_days_since_modification_greater_than = 60
+        delete_after_days_since_modification_greater_than          = 120
+      }
+    }
+  }
 }

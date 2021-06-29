a8eb84ac6e27b3d773e7d0d0266cf08809292cc0
thecomalley <31399219+thecomalley@users.noreply.github.com>
2021-06-29 14:44:14 +1000

    set PING_URL from variable

--- rclone/rclone-appdata.sh
+++ rclone/rclone-appdata.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# Variables
+PING_URL="https://hc-ping.com/bf0942b7-b8be-4dac-90e7-f350b257a708"
+
 # tell healthchecks job started
-curl -m 10 --retry 5 https://hc-ping.com/eb095278-f28d-448d-87fb-7b75c171a6aa/start
+curl -m 10 --retry 5 $PING_URL/start
 
 # Copy the source to the destination. Doesn't transfer unchanged files. Doesn't delete files from the destination
-rclone copy /mnt/user/Backup/appdata azure-remote-backup:appdata --dry-run
+msg=$(rclone copy /mnt/user/Backup/appdata azure-remote-backup:appdata --dry-run)
+rclone_exit = $?
 
 # tell healthchecks job completed
-curl -m 10 --retry 5 https://hc-ping.com/eb095278-f28d-448d-87fb-7b75c171a6aa
+curl -m 10 --retry 5 --data-raw "$msg" $PING_URL/$?

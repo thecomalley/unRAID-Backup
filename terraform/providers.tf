da0fb81cf849e7624f66019449359b21c5f109ca
Chris O'Malley <31399219+thecomalley@users.noreply.github.com>
2023-02-17 09:30:54 +1300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.65.0"
     }
     healthchecksio = {
       source  = "kristofferahl/healthchecksio"
-      version = "1.9.0"
     }
   }
 }

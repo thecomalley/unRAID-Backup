fd19843a1da521c7c41fbab2aee415d83bcf73f4
Chris O'Malley <31399219+thecomalley@users.noreply.github.com>
2023-02-17 09:49:14 +1300

    refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,37 +3,41 @@ module "naming" {
   suffix = ["unRAID", "backup"]
 }
 
-locals {
-  common_tags = {
-    application = "unRAID-backup"
-    enviroment  = "production"
-    deployment  = "terraform"
-  }
-}
-
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "main" {
   name     = module.naming.resource_group.name
   location = var.location
-  tags = merge(local.common_tags, {
-    yor_trace = "fd7d90bf-3fa5-4025-8908-c7c4341fc2d2"
-    }, {
-    git_commit           = "0879e5f930fb37e329cbe2536e368df32c2bc9f5"
-    git_file             = "terraform/main.tf"
-    git_last_modified_at = "2021-06-28 16:37:17"
-    git_last_modified_by = "31399219+thecomalley@users.noreply.github.com"
-    git_modifiers        = "31399219+thecomalley"
-    git_org              = "thecomalley"
-    git_repo             = "unraid-Backup"
-  })
 }
 
-resource "template_dir" "config" {
-  source_dir      = "../rclone/templates"
-  destination_dir = "../rclone/user_scripts"
+resource "azurerm_storage_account" "main" {
+  name                     = module.naming.storage_account.name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-  vars = {
-    ping_url           = "${healthchecksio_check.appdata.ping_url}"
-    storage_account    = "${azurerm_storage_account.example.name}"
-    primary_access_key = "${azurerm_storage_account.example.primary_access_key}"
+}
+
+resource "azurerm_storage_container" "main" {
+  name                  = "appdata"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_management_policy" "main" {
+  storage_account_id = azurerm_storage_account.main.id
+  rule {
+    name    = "global-policy"
+    enabled = true
+    filters {
+      prefix_match = ["*"]
+      blob_types   = ["blockBlob"]
+    }
+    actions {
+      base_blob {
+        tier_to_cool_after_days_since_modification_greater_than    = 30
+        tier_to_archive_after_days_since_modification_greater_than = 60
+        delete_after_days_since_modification_greater_than          = 120
+      }
+    }
   }
 }

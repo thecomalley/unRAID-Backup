fed02985261bebfa78264f04f0a0cf9c5fbf2c0a
thecomalley <31399219+thecomalley@users.noreply.github.com>
2023-02-17 12:38:25 +1300

    output

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,14 @@
-output "PING_URL" {
+output "ping_url" {
   value       = healthchecksio_check.appdata.ping_url
   description = "Ping URL associated with this check"
+}
+
+output "storage_account_name" {
+  value       = azurerm_storage_account.main.name
+  description = "The name of the storage account"
+}
+
+output "storage_account_key" {
+  value       = azurerm_storage_account.main.primary_access_key
+  description = "The primary access key for the storage account"
 }
